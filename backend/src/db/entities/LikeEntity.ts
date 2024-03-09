85c883bdf6f83d05a93774b5746ab07a9f8951af
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-03-09 12:18:24 +0530

    chore(backend): Remove index in like entity

--- backend/src/db/entities/LikeEntity.ts
+++ backend/src/db/entities/LikeEntity.ts
@@ -1,7 +1,6 @@
 import { CreateDateColumn, Entity, Index, PrimaryColumn } from "typeorm";
 
 @Entity()
-@Index(["postId", "profileId"], { unique: true })
 export class Like {
     @PrimaryColumn({ name: "post_id" })
     postId: string;
