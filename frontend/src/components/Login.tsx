70f1f139b3752f85091fd8df695f147238fb9643
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-03-10 20:04:25 +0530

    feat(frontend): Add logic to open the login modal on click

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,121 @@
+import { FC, useMemo, useState } from "react";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    Input,
+} from "@nextui-org/react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+
+interface LoginProps {
+    isOpen: boolean;
+    onOpenChange: () => void;
+}
+
+const Login: FC<LoginProps> = props => {
+    const { isOpen, onOpenChange } = props;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const validateEmail = (value: string) =>
+        value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+
+    const isInvalidEmail = useMemo(() => {
+        if (email === "") return false;
+
+        return validateEmail(email) ? false : true;
+    }, [email]);
+
+    const isInvalidPassword = useMemo(() => {
+        if (password === "") return false;
+
+        return password.length < 8;
+    }, [password]);
+
+    return (
+        <>
+            <Modal
+                backdrop="blur"
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                placement="top-center">
+                <ModalContent>
+                    {onClose => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-1">
+                                Log in
+                            </ModalHeader>
+                            <ModalBody>
+                                <Input
+                                    autoFocus
+                                    required
+                                    size={"lg"}
+                                    placeholder="Enter your email"
+                                    variant="bordered"
+                                    value={email}
+                                    type="email"
+                                    isInvalid={isInvalidEmail}
+                                    color={
+                                        isInvalidEmail ? "danger" : undefined
+                                    }
+                                    errorMessage={
+                                        isInvalidEmail &&
+                                        "Please enter a valid email"
+                                    }
+                                    onValueChange={setEmail}
+                                />
+                                <Input
+                                    size={"lg"}
+                                    placeholder="Enter your password"
+                                    endContent={
+                                        <button
+                                            type="button"
+                                            onClick={toggleVisibility}>
+                                            {isVisible ? (
+                                                <IoMdEyeOff size={20} />
+                                            ) : (
+                                                <IoMdEye size={20} />
+                                            )}
+                                        </button>
+                                    }
+                                    type={isVisible ? "text" : "password"}
+                                    variant="bordered"
+                                    required
+                                    value={password}
+                                    isInvalid={isInvalidPassword}
+                                    color={
+                                        isInvalidPassword ? "danger" : undefined
+                                    }
+                                    errorMessage={
+                                        isInvalidPassword &&
+                                        "Password must be at least 8 characters"
+                                    }
+                                    onValueChange={setPassword}
+                                />
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button
+                                    color="danger"
+                                    variant="flat"
+                                    onPress={onClose}>
+                                    Close
+                                </Button>
+                                <Button color="primary" onPress={onClose}>
+                                    Sign in
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
+
+export default Login;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,37 +6,43 @@ import {
     NavbarItem,
     Button,
     Image,
-    Link,
+    useDisclosure,
 } from "@nextui-org/react";
 import MaskmateLogo from "@/assets/logo.png";
+import Login from "./Login";
 
 interface NavigationBarProps {}
 
 const navbar: FC<NavigationBarProps> = ({}) => {
+    const { isOpen, onOpen, onOpenChange } = useDisclosure();
     return (
-        <Navbar>
-            <NavbarBrand>
-                <Image src={MaskmateLogo} alt="logo" width={40} />
-            </NavbarBrand>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Button
-                        color="primary"
-                        className="font-semibold"
-                        variant="bordered">
-                        Login
-                    </Button>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button
-                        color="primary"
-                        className="text-white font-semibold"
-                        variant="solid">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+        <>
+            <Login isOpen={isOpen} onOpenChange={onOpenChange} />
+            <Navbar>
+                <NavbarBrand>
+                    <Image src={MaskmateLogo} alt="logo" width={40} />
+                </NavbarBrand>
+                <NavbarContent justify="end">
+                    <NavbarItem className="hidden lg:flex">
+                        <Button
+                            onPress={onOpen}
+                            color="primary"
+                            className="font-semibold"
+                            variant="bordered">
+                            Login
+                        </Button>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Button
+                            color="primary"
+                            className="text-white font-semibold"
+                            variant="solid">
+                            Sign Up
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+        </>
     );
 };
 
